067d3d09202e7327f5ecda33cae78f1b4b610c08
Hoangs0 <hoang_lp_97@yahoo.com>
2017-07-10 03:58:20 +0300

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express= require ('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {user} = require('./models/user');
@@ -36,13 +37,29 @@ app.post('/todos', (req, res) => {
     });
 }); */
 
-app.get('/todos', (req,res) => {
+/* app.get('/todos', (req,res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     })
-});
+}); */
+
+// GET /todos/12313432
+app.get('/todos/:id', (req,res) => {
+    var id= req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+        
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.startus(404).send();
+        }
+        res.send(JSON.stringify(todo,undefined, 2));
+    }, (e) => console.log(res.status(400).send()));
+})
 
 app.listen(3000, () => {
     console.log('Startd on port 3000');
