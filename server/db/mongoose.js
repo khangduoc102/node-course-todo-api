196905fa16a8ad67a1ecaabbd7b04265229e0b22
Hoangs0 <hoang_lp_97@yahoo.com>
2017-07-12 23:53:40 +0300

    test2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.PORT || 'mongodb://hoangle:hoanglp97@ds157112.mlab.com:57112/my-node-course');
+mongoose.connect("mongodb://hoangle:hoanglp97@ds157112.mlab.com:57112/my-node-course");
 
 module.exports = {mongoose};
