57f1ce4b1a26938221f421a0a54531568e123b88
Hoangs0 <hoang_lp_97@yahoo.com>
2017-07-13 00:42:59 +0300

    test4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://hoangle:hoanglp97@ds157112.mlab.com:57112/my-node-course'
 };
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect( db.mlab || db.localhost);
 
 module.exports = {mongoose};
