61c41e248cb7a24fc5a29d28b249b0834f256760
Hoangs0 <hoang_lp_97@yahoo.com>
2017-07-14 15:38:15 +0300

    add PATCH

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express= require ('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express= require ('express');
+const bodyParser = require('body-parser');
 
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
@@ -76,6 +77,32 @@ app.delete('/todos/:id', (req, res) => {
         res.send({todo});
     }, (e) => console.log(res.status(400).send()));
 })
+
+// PATCH
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+     if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }   else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`Started up on ${port}`);
